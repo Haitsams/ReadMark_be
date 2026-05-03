a93c7d73ea810b124ec7303792b3210c20b7cbfc
Haitsams <harphypaw.09@gmail.com>
2026-05-03 22:52:17 +0700

    fix

--- Bookmark_be/routes/bookmark.js
+++ Bookmark_be/routes/bookmark.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Bookmark = require("../models/Bookmark");
+const Bookmark = require("../models/bookmark");
 
 router.post("/", async (req, res) => {
     try {
